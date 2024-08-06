5f52ac7f268006b2ab946b42470f4b0794d8b5b5
Kirk Hofer <khofer@gmail.com>
2024-08-06 16:53:45 -0500

    sql tester updates

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 Repo to share things I have learned over my many years of programming and data experiences
 
 # Captain's Log
+## 2024-08-06
+- 🆙 [SQL Tester](sql/README.md#sql-tester) added some code to output the total sessions in use
+
 ## 2024-06-25
 - 🆕 [Fabric APIs](fabric/README.md#fabric-apis) notebook and all
 
--- sql/sql-tester.ps1
+++ sql/sql-tester.ps1
@@ -9,10 +9,10 @@
 
 
     Install-Module -Name Az -AllowClobber -Scope AllUsers
-    . '/home/kirk/code/playbook/posh/sql-tester.ps1' -MaxPoolSize 10 -RunLimit 20 -RunType "Sequential" -CloseConnection $false
-    . '/home/kirk/code/playbook/posh/sql-tester.ps1' -MaxPoolSize 10 -RunLimit 100 -RunType "Parallel" -CloseConnection $true
-    . '/home/kirk/code/playbook/posh/sql-tester.ps1' -MaxPoolSize 0 -RunLimit 100 -RunType "Parallel" -CloseConnection $true
-    . '/home/kirk/code/playbook/posh/sql-tester.ps1' -MaxPoolSize 0 -RunLimit 100 -RunType "Parallel" -CloseConnection $false
+    . '/home/kirk/code/playbook/posh/sql-tester.ps1' -MaxPoolSize 10 -RunLimit 20 -MaxThreads 1 -CloseConnection $false
+    . '/home/kirk/code/playbook/posh/sql-tester.ps1' -MaxPoolSize 10 -RunLimit 100 -MaxThreads 20 -CloseConnection $true
+    . '/home/kirk/code/playbook/posh/sql-tester.ps1' -MaxPoolSize 0 -RunLimit 100 -MaxThreads 20 -CloseConnection $true
+    . '/home/kirk/code/playbook/posh/sql-tester.ps1' -MaxPoolSize 0 -RunLimit 100 -MaxThreads 20 -CloseConnection $false
 
     .EXAMPLE This will error out because of connections in the pool aren't there
     PS> . '/home/kirk/code/playbook/posh/sql-tester.ps1' -RunLimit 100 -ConnectionTimeout 2 -CloseConnection $false
@@ -20,17 +20,17 @@
 #requires -Modules Az.Accounts
 [CmdletBinding()]
 param(
-    [string]$ServerInstance="sql-dai-dev-use.database.windows.net",
+    [string]$serverInstance="sql-dai-dev-use.database.windows.net",
     [string]$Database="SampleDB",
-    [int]$RunLimit=10,
-    [ValidateSet("Sequential", "Parallel")]
-    [string]$RunType="Parallel",
+    [int]$RunLimit=100,
     [int]$MaxThreads=20,
     [double]$SleepSeconds=.5,
     [int]$MaxPoolSize=0,
-    [int]$ConnectionTimeout=5,
+    [int]$MinPoolSize=0,
+    [int]$ConnectionTimeout=10,
     [bool]$CloseConnection=$true,
-    [string]$AppPrefix="sql-tester-"
+    [string]$AppPrefix="sql-tester-",
+    [switch]$ShowDetail
 )
 $ErrorActionPreference = "Stop"
 
@@ -41,111 +41,140 @@ if( $null -eq (Get-AzContext) -or (-not $accessToken) -or ([DateTime]::UtcNow -g
     $token = $accessToken.Token
 }
 
-$appName = "$AppPrefix$(($CloseConnection) ? 'close' : 'open')"
-$connectionString = "Server=tcp:$ServerInstance,1433;Initial Catalog=$Database;Connection Timeout=$ConnectionTimeout;Application Name=$appName" 
+
+$appName = "$AppPrefix$(($CloseConnection) ? 'close' : 'open')-$([datetime]::UtcNow.ToString("yyyyMMddHHmmssfff"))"
+#$appName = "$AppPrefix$(($CloseConnection) ? 'close' : 'open')"
+$connectionString = "Server=tcp:$ServerInstance,1433;Initial Catalog=$Database;Connection Timeout=$ConnectionTimeout;Application Name=$appName;" 
 if ( $MaxPoolSize -gt 0 )
 {
-    $connectionString += ";Max Pool Size=$MaxPoolSize;"
+    $connectionString += "Max Pool Size=$MaxPoolSize;"
 }
-if( $RunType -eq "Sequential" )
+if( $MaxPoolSize -gt $MinPoolSize -or ($MaxPoolSize -eq 0 -and $MinPoolSize -gt 0) )
 {
-    $i=0
-    while($i -lt $RunLimit)
-    {
-        $conn = New-Object System.Data.SqlClient.SQLConnection 
-        $conn.ConnectionString = $connectionString
-        $conn.AccessToken = $token
-        try
-        {
-            $conn.Open()
-            $cmd = $conn.CreateCommand()
-            $cmd.CommandText = "SELECT GETDATE() AS dt"
-            $dt = [datetime]$cmd.ExecuteScalar()
-            Write-Output "Run: $i Time: $dt"
-        }
-        catch
-        {
-            Write-Output "Run: $i Error: $($_.Exception.Message)"
-        }
-        finally 
-        {
-            $cmd.Dispose(); 
-            if($CloseConnection)
-            {
-                $conn.Close(); 
-            }
-        }
+    $connectionString += "Min Pool Size=$MinPoolSize;"
+}
 
-        $i++
+$items=@()
+1..$RunLimit|ForEach-Object {
+
+    $items+=@{
+        id = $_
     }
 }
-elseif( $RunType -eq "Parallel" )
-{
-    $items=@()
-    1..$RunLimit|ForEach-Object {
 
-        $items+=@{
-            id = $_
-        }
-    }
-    
-    # Create a hashtable for process.
-    # Keys should be ID's of the processes
-    $origin = @{}
-    $items | Foreach-Object {$origin.($_.id) = @{}}
-    $sync = [System.Collections.Hashtable]::Synchronized($origin)
+# Create a hashtable for process.
+# Keys should be ID's of the processes
+$origin = @{}
+$items | Foreach-Object {$origin.($_.id) = @{}}
+$sync = [System.Collections.Hashtable]::Synchronized($origin)
 
-    $job = $items | Foreach-Object -ThrottleLimit $MaxThreads -AsJob -Parallel {
-        $syncCopy = $using:sync
-        $process = $syncCopy.$($PSItem.Id)
+$job = $items | Foreach-Object -ThrottleLimit $MaxThreads -AsJob -Parallel {
+    $syncCopy = $using:sync
+    $process = $syncCopy.$($PSItem.Id)
 
 
-        $startTime = ([DateTime]::UtcNow)
+    $startTime = ([DateTime]::UtcNow)
 
-        $process.Id = $PSItem.id
-        $process.Status = "Running"
-        $process.StartTime=$startTime
-    
-        $conn = New-Object System.Data.SqlClient.SQLConnection 
-        $conn.ConnectionString = $using:connectionString
-        $conn.AccessToken = $using:token
-        try
-        {
-            $conn.Open()
-            $process.Status = "Complete"
-            $cmd = $conn.CreateCommand()
-            $cmd.CommandText = "SELECT GETDATE() AS dt"
-            $process.Results = [datetime]$cmd.ExecuteScalar()
-        }
-        catch
-        {
-            $process.Status = "Error"
-            $process.Results= $_.Exception.Message
-        }
-        finally 
-        {
-            $cmd.Dispose(); 
-            if($using:CloseConnection)
-            {
-                $conn.Close(); 
-            }
-        }
-        $ts = New-TimeSpan -Start $startTime -End ([DateTime]::UtcNow)    
-        $process.TotalSeconds = $ts.TotalSeconds
-    }
+    $process.Id = $PSItem.id
+    $process.Status = "Running"
+    $process.StartTime=$startTime
 
-    while($job.State -eq 'Running')
+    $conn = New-Object System.Data.SqlClient.SQLConnection 
+    $conn.ConnectionString = $using:connectionString
+    $conn.AccessToken = $using:token
+    try
     {
-        $run = ($sync.Keys | ?{$sync.$_.Status -eq "Running"}).length
-        $cnt = ($sync.Keys | ?{$sync.$_.Status -and $sync.$_.Status -ne "Running"}).length
-        Write-Host "$([datetime]::utcnow.ToString("HH:mm:ss.fff")): Running...$($run) Completed...$($cnt)"
-        # Wait to refresh to not overload gui
-        Start-Sleep -Seconds $SleepSeconds
-    }    
+        $conn.Open()
+        $process.Status = "Complete"
+        $cmd = $conn.CreateCommand()
+        $cmd.CommandText = "WAITFOR DELAY '00:00:01';SELECT FORMAT(SYSDATETIME(), 'yyyy-MM-dd HH:mm:ss.fff') AS dt;"
+        $process.Results = $cmd.ExecuteScalar()
+    }
+    catch
+    {
+        # Write-Output "Error connecting to $ServerInstance on $i"
+        # Write-Output $_.Exception.Message
+        $process.Status = "Error"
+        $process.Results= $_.Exception.Message
+    }
+    finally 
+    {
+        # $cmd.Close();
+        $cmd.Dispose(); 
+        if($using:CloseConnection)
+        {
+            $conn.Close(); 
+        }
+    }
+    $ts = New-TimeSpan -Start $startTime -End ([DateTime]::UtcNow)    
+    $process.TotalSeconds = $ts.TotalSeconds
+}
+
+while($job.State -eq 'Running')
+{
+    # $sync.Keys | Foreach-Object {
+    #     # If key is not defined, ignore
+    #     if(![string]::IsNullOrEmpty($sync.$_.keys))
+    #     {
+    #         if( -not $sync.$_.Completed )
+    #         {
+    #             Write-Host "Id: $($sync.$_.Id)"
+    #         }
+    #     }
+    # }
+    $run = ($sync.Keys | ?{$sync.$_.Status -eq "Running"}).length
+    $cnt = ($sync.Keys | ?{$sync.$_.Status -and $sync.$_.Status -ne "Running"}).length
+    
+    
+    $sql = "
+    SELECT COUNT(*) AS session_count
+    FROM sys.dm_exec_sessions
+    WHERE program_name = '$appName';
+    "
+    $resultSql=Invoke-SqlCmd -ServerInstance $serverInstance -database $Database -query $sql -AccessToken $token
+
+    $sessionCount = 0
+    if( $resultSql )
+    {
+        $sessionCount=[int]$resultSql.session_count
+    }
+    # Write-Host "Session Count: $($resultSql.session_count) LastRequest_Minutes: $($resultSql.LastRequest_Minutes)"
+    Write-Host "$([datetime]::utcnow.ToString("HH:mm:ss.fff")): Running...$($run) Completed...$($cnt) Sessions...$sessionCount"
+    # Write-Host "$([datetime]::utcnow.ToString("HH:mm:ss.fff")): Running...$($run) Completed...$($cnt)"
+    
+    # Wait to refresh to not overload gui
+    Start-Sleep -Seconds $SleepSeconds
+}    
+if( $ShowDetail )
+{
     $sync.Keys | Foreach-Object {
+        # $c=$items|Where-Object id -eq $sync.$_.Id
         Write-Host "Id: $($sync.$_.Id) StartTime: $($sync.$_.StartTime.ToString("HH:mm:ss.fff")) TotalSeconds: $($sync.$_.TotalSeconds) Status: $($sync.$_.Status) Results: $($sync.$_.Results)"
     }
-
-    ($sync.Keys | %{$c=$items|Where-Object id -eq $sync.$_.Id;"{0}" -f $sync.$_.Status}) | Group-Object
 }
+($sync.Keys | %{$c=$items|Where-Object id -eq $sync.$_.Id;"{0}" -f $sync.$_.Status}) | Group-Object
+
 Write-Host $connectionString
+
+$sql = "
+SELECT program_name,
+    status,
+    COUNT(session_id) AS session_count
+    ,MAX(DATEDIFF(minute,last_request_end_time,GETDATE())) AS MaxRequest_Minutes
+    ,MIN(DATEDIFF(minute,last_request_end_time,GETDATE())) AS MinRequest_Minutes
+FROM sys.dm_exec_sessions
+WHERE program_name = '$appName'
+GROUP BY program_name,status;
+"
+$resultSql=Invoke-SqlCmd -ServerInstance $serverInstance -database $Database -query $sql -AccessToken $token
+if( $resultSql )
+{
+    Write-Host "Session Count: $($resultSql.session_count) MaxRequest_Minutes: $($resultSql.MaxRequest_Minutes) MinRequest_Minutes: $($resultSql.MinRequest_Minutes)"
+}
+else
+{
+    Write-Host "No Sessions found for $appName"
+}
+
+# Clear all the pools
+[System.Data.SqlClient.SQLConnection]::ClearAllPools()
